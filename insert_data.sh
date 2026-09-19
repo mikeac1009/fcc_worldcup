e71e4f2a607d4a2831dbf39598550c4ebcd99366
mikeac1009 <166648685+mikeac1009@users.noreply.github.com>
2026-09-19 17:09:07 -0400

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,40 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+# Script to insert data into worldcup database
+echo $($PSQL "TRUNCATE TABLE games, teams")
+
+#filling in games and teams table
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != year ]]
+  then
+    #insert winner into teams table
+    #get team_id
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    #if not found
+    if [[ -z $TEAM_ID ]]
+    then
+      #insert winner
+      INSERT_WINNER=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+    fi
+    
+    #insert opponent into teams table
+    #get team_id
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    #if not found
+    if [[ -z $TEAM_ID ]]
+    then
+      #insert opponent
+      INSERT_OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+    fi
+
+    #get winner_id
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    #get opponent_id
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    #insert game
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+  fi
+done
